9392cdab1bc1a911626ddcd982ffc5f97e79f4be
GFox <Foxxdim@mail.ru>
2022-04-22 18:21:27 +0300

    Created all fragments 1,2,3,3_2, navigation_graph.xml,

--- app/src/main/java/com/fox/interviewassistanceapp/domain/usecase/SetAllQuestionUseCase.kt
+++ app/src/main/java/com/fox/interviewassistanceapp/domain/usecase/SetAllQuestionUseCase.kt
@@ -13,11 +13,11 @@ class SetAllQuestionUseCase() {
 //        )
 //    }
 
-    val allQuestions: List<QuestionModel>
-            = listOf(
-            QuestionModel(1, "Вопрос 1", "Ответ 1"),
-            QuestionModel(2, "Вопрос 2", "Ответ 2"),
-            QuestionModel(3, "Вопрос 3", "Ответ 3"))
+//    val allQuestions: List<QuestionModel>
+//            = listOf(
+//            QuestionModel(1, "Вопрос 1", "Ответ 1"),
+//            QuestionModel(2, "Вопрос 2", "Ответ 2"),
+//            QuestionModel(3, "Вопрос 3", "Ответ 3"))
 
 
 
